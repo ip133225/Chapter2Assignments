69a182482c1a7b2c8ec595b7624214c00177ae6b
Isaiah Ponciano <zai.cruz@rocketmail.com>
2017-02-08 13:35:27 -0600

    update

---
+++ CarlysMotto/src/CarlyMotto/CarleysMotto.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package CarlyMotto;
+
+/**
+ * @author IP133225
+ *
+ */
+public class CarleysMotto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Carleys makes the food that makes the party"); 
+		
+	}
+
+}
---
+++ movieQuote/movieQuote.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package movieQuote;
+
+/**
+ * @author IP133225
+ *
+ */
+public class movieQuote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Frankly, my dear, I don't give a damn");
+	}
+
+}
